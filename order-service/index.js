ba05caf118e8f7c5ec0aa2500ba1819943353060
valentinacamacho <valentinacamacho0206@gmail.com>
2025-06-10 18:08:28 -0500

    Fix: Corrected 'ampqlib' to 'amqplib' in order-service. This resolves the 'MODULE_NOT_FOUND' error preventing the order-service from starting.Also addressed Docker connection issues (Docker Engine stopped & admin privileges for terminal)during debugging setup.

--- order-service/index.js
+++ order-service/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const amqp = require("ampqlib");
+const amqp = require("amqplib");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,43 +9,54 @@ const PORT = process.env.PORT || 3000;
 let connection;
 let channelRabbit;
 
-async function connectRabbitMQ(){
-    try{
+async function connectRabbitMQ() {
+    try {
         connection = await amqp.connect(process.env.RABBITMQ_URL);
         channelRabbit = await connection.createChannel();
 
         console.log("Order Service: Conectado a RabbitMQ con éxito");
-        
-    } catch (error){
+        await channelRabbit.assertQueue(process.env.ORDER_CREATED_QUEUE, { durable: true });
+        console.log(`Order Service: Cola '${process.env.ORDER_CREATED_QUEUE}' cola declarada y lista para enviar.`)
+
+        await channelRabbit.assertQueue(process.env.ORDER_STATUS_UPDATE_QUEUE, { durable: true });
+        console.log(`Order Service: Cola' ${process.env.ORDER_STATUS_UPDATE_QUEUE}' Cola declarada y lista para recibir `);
+
+         connection.on("error", (err) => {
+            console.error("Order Service: Error en la conexión a RabbitMQ:", err.message);
+        });
+
+    } catch (error) {
         console.log("Order Service: Error al conectar a RabbitMQ", error.message);
+        console.log('Order Service: Reintentando conexión a RabbitMQ en 5 segundos...');
+        setTimeout(connectRabbitMQ, 10000); 
     }
 }
 
-async function iniciarServices(){
-   console.log("Order Service: Iniciando Servidor");
-   
-   await connectRabbitMQ();
-   app.use(express.json());
+async function iniciarServices() {
+    console.log("Order Service: Iniciando Servidor");
 
-   app.get("/",(req, res) => {
-    let rabbitmqStatus = "Desconocido";
-    if(connection && connection.isConnected()){
-        rabbitmqStatus = "Conectado";
-    }else {
-        rabbitmqStatus =" No iniciada"
-    }
-    res.status(200).send("El servidor ORDER SERVICE esta escucahando peticiones ");
-   });
+    await connectRabbitMQ();
+    app.use(express.json());
 
-   app.listen(PORT, () =>{
-    console.log(`Order Service: Escuchando  http://localhost:${PORT}`);
-   });
+    app.get("/", (req, res) => {
+        let rabbitmqStatus = "Desconocido";
+        if (connection && connection.isConnected()) {
+            rabbitmqStatus = "Conectado";
+        } else {
+            rabbitmqStatus = " No iniciada"
+        }
+        res.status(200).send("El servidor ORDER SERVICE esta escucahando peticiones ");
+    });
 
-   console.log("Esta listo para recibir conexiones de HTTP");
+    app.listen(PORT, () => {
+        console.log(`Order Service: Escuchando  http://localhost:${PORT}`);
+    });
+
+    console.log("Esta listo para recibir conexiones de HTTP");
 }
 
 iniciarServices();
 
-process.on("uncaughtException", (err) =>{
-    console.error("Error de excepción"),error;
+process.on("uncaughtException", (err) => {
+    console.error("Error de excepción"), error;
 })
