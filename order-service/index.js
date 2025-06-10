3de5adc7fcd89fd581331ca4f6d2407343c22b4d
valentinacamacho <valentinacamacho0206@gmail.com>
2025-06-10 12:12:07 -0500

    feat: order-service adds asynchronous connection to RabbitMQ. It enables queue management (FIFO) for efficient communication, although the business logic is not yet implemented.

--- order-service/index.js
+++ order-service/index.js
@@ -1,20 +1,43 @@
 require("dotenv").config();
 
 const express = require("express");
-
+const amqp = require("ampqlib");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let connection;
+let channelRabbit;
+
+async function connectRabbitMQ(){
+    try{
+      
+        channelRabbit = await connection.createcChannel();
+
+        console.log("Order Service: Conectado a RabbitMQ con éxito");
+    } catch (error){
+        console.log("Order Service: Error al conectar a RabbitMQ", error.message);
+    }
+}
+
 async function iniciarServices(){
-   console.log("Iniciando servicio - Order service");
+   console.log("Order Service: Iniciando Servidor");
    
-   app.get("/",(req, res) =>{
+   await connectRabbitMQ();
+   app.use(express.json());
+
+   app.get("/",(req, res) => {
+    let rabbitmqStatus = "Desconocido";
+    if(connection && connection.isConnected()){
+        rabbitmqStatus = "Conectado";
+    }else {
+        rabbitmqStatus =" No iniciada"
+    }
     res.status(200).send("El servidor ORDER SERVICE esta escucahando peticiones ");
    });
 
    app.listen(PORT, () =>{
-    console.log(`Escuchando en en http://localhost:${PORT}`);
+    console.log(`Order Service: Escuchando  http://localhost:${PORT}`);
    });
 
    console.log("Esta listo para recibir conexiones de HTTP");
